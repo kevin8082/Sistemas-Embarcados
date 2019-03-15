afb319d2c510bb6a664e82fa1d928576dcd67224
Kevin <kevinho8866@hotmail.com>
2019-03-14 22:17:46 -0300

    Create main.c

---
+++ Aula 3/Pisca3Led/main.c
@@ -0,0 +1,21 @@
+#include <tm4c123gh6pm.h>
+#include <inttypes.h>
+
+int main(void)
+{
+    uint32_t aux;
+    SYSCTL_RCGCGPIO_R = 0x20;
+    GPIO_PORTF_DIR_R  = 0x0E;
+    GPIO_PORTF_DEN_R  = 0x0E;
+
+    while(1){
+        GPIO_PORTF_DATA_R = 0x08; //ligado
+        for(aux = 0; aux < 2000000; aux++){}
+        GPIO_PORTF_DATA_R = 0x04; //ligado
+        for(aux = 0; aux < 2000000; aux++){}
+        GPIO_PORTF_DATA_R = 0x02; //ligado
+        for(aux = 0; aux < 2000000; aux++){}
+    }
+
+	return 0;
+}
