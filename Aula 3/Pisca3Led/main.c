7921cac8223bb6b971432190607cc57d625dd725
Kevin <kevinho8866@hotmail.com>
2019-03-21 19:46:21 -0300

    Update main.c

--- Aula 3/Pisca3Led/main.c
+++ Aula 3/Pisca3Led/main.c
@@ -5,8 +5,8 @@ int main(void)
 {
     uint32_t aux;
     SYSCTL_RCGCGPIO_R = 0x20;
-    GPIO_PORTF_DIR_R  = 0x0E;
-    GPIO_PORTF_DEN_R  = 0x0E;
+    GPIO_PORTF_DIR_R = 0x0E;
+    GPIO_PORTF_DEN_R = 0x0E;
 
     while(1){
         GPIO_PORTF_DATA_R = 0x08; //ligado
