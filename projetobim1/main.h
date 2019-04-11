5f5cffc3adee65ad950208cce0a9a80ba25b5db7
Kevin <kevinho8866@hotmail.com>
2019-04-11 19:12:49 -0300

    Create main.h

---
+++ projetobim1/main.h
@@ -0,0 +1,24 @@
+/*
+ * main.h
+ *
+ *  Created on: 7 de abr de 2019
+ *      Author: kevin
+ */
+#include <inttypes.h>
+#ifndef MAIN_H_
+#define MAIN_H_
+
+
+uint32_t level = 0;
+uint32_t mostrarOrdem = 0;
+uint32_t entradaOrdem = 0;
+uint32_t cores[20] = {0};
+uint32_t ultimoBotao = 0;
+uint32_t count1 = 0;
+uint32_t count2 = 0;
+uint32_t count3 = 0;
+uint32_t entrada = 0;
+uint32_t posAtual = 0;
+uint32_t novaCor = 1;
+uint32_t controleCor = 0;
+#endif /* MAIN_H_ */
